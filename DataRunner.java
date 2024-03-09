e0e3c10e0b4ba17ff8fc2bb854a8b34e213f5aa9
Cinderblock123 <153293909+DeepFryer123@users.noreply.github.com>
2024-03-09 13:53:51 -0500

    Update DataRunner.java

--- DataRunner.java
+++ DataRunner.java
@@ -1,7 +1,8 @@
 public class DataRunner {
     public static void main(String[] args){
         //Where all the running will happen
-        
+        for(int i=0;i<1000000000;i++){
         System.out.println("yuh");
+        }
     }
 }
