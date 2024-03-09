b15d31bfe0595889d67086d9bbaa10f77e1eb289
Cinderblock123 <153293909+DeepFryer123@users.noreply.github.com>
2024-03-09 13:39:21 -0500

    Update DataRunner.java

--- DataRunner.java
+++ DataRunner.java
@@ -2,6 +2,6 @@ public class DataRunner {
     public static void main(String[] args){
         //Where all the running will happen
         
-        System.out.println("NATHAN CHAN GET YOUR ASS ONLINE RIGHT NOW");
+        System.out.println("yuh");
     }
 }
