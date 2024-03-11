d51bcf671ed3b144c2d9cda771d561692c6542f3
Nathan <nathan5501@gmail.com>
2024-03-11 11:16:59 -0400

    forntite

--- DataRunner.java
+++ DataRunner.java
@@ -1,5 +1,5 @@
 //import org.code.theater.*;
-import java.util.Scanner;
+import java.util.*;
 public class DataRunner {
     public static void main(String[] args){
         //Where all the running will happen
@@ -8,6 +8,50 @@ public class DataRunner {
         
         Scanner userChoice = new Scanner(System.in);
         
+        /* we are going to be using the user choice
+         * This is going to check what the user is picking for their graph / years they want to see
+         * We are going to build the conditionals as a debugging structure, 
+         * however we need to decide what we are doing for the project.
+         */
+        System.out.println("Please state which graph you would like to see: CO2, Ice, Temp"); 
+        String choice = userChoice.nextLine();
+        
+        
+        if(choice.equals("CO2")){
+            System.out.println("How many years would you like to go back?");
+            userChoice.nextInt();
+            //whatever(choice);
+            }
+
+        
+
+        if(choice.equals("Ice")){
+            System.out.println("How many years would you like to go back?");
+            userChoice.nextInt();
+            //whatever(choice);
+            }
+
+        
+       
+        if(choice.equals("Ping") || choice.equals("ping")){
+            System.out.println("pong");
+        }
+        
+
+
+        userChoice.close();
+    }
+}
+//import org.code.theater.*;
+import java.util.Scanner;
+public class DataRunner {
+    public static void main(String[] args){
+        //Where all the running will happen
+        System.out.println("\n\n\nData Visusalisation project on climate change.\n\n\n");
+        //we are doing the project on the tons of CO2 in the atmosphere per year over the past 100 year
+        
+        Scanner userChoice = new Scanner(System.in);
+
         /* we are going to be using the user choice
          * This is going to check what the user is picking for their graph / years they want to see
          * We are going to build the conditionals as a debugging structure, 
@@ -32,12 +76,14 @@ public class DataRunner {
             }
 
         
-        
-       
 
+        if(choice.equals("Temp")){
+            System.out.println("How many years would you like to go back?");
+            userChoice.nextInt();
+            //whatever(choice);
+            }
 
         userChoice.close();
         }
     
 }
- 
