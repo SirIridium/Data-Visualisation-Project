e77e553cbe1fb5236bd5db3d9d1d78e9f5194285
SirIridium <127507341+SirIridium@users.noreply.github.com>
2024-03-11 11:12:28 -0400

    I modifed the files

--- Display.java
+++ Display.java
@@ -1,16 +1,16 @@
 //remember that this extends the scene. 
 public class Display /*extends scene*/{
     private static final int YEAR = 2024;
-    private int yearsToGoBack; 
-    
+    private int daysToGoBack; 
+
     //constructor
     public Display(int yearsToGoBack){
-        this.yearsToGoBack = yearsToGoBack;
+        this.daysToGoBack = yearsToGoBack;
     }
 
     //returns the yearsToGoBack
     public int getYearsToGoBack(){
-        return yearsToGoBack; 
+        return daysToGoBack; 
     }
 
 
--- Temp.java
+++ Temp.java
@@ -1,16 +1,20 @@
 public class Temp extends Display {
     //this is the class that will represent the COTwo per year
-    private int yearsToGoBack;
-    private double[] tons = new double[yearsToGoBack];
+    private int daysToGoBack;
+    private double[] temp = new double[daysToGoBack];
 
 
 
     //the tons array will be made with the text file of all the tons.
-    public Temp(int yearsToGoBack, double[] tons){
+    public Temp(int yearsToGoBack, double[] temp){
         super(yearsToGoBack);
-        this.tons = tons;
+        this.temp = temp;
     }
 
-    //public 
+    public double[] createTemp(){
+        double[] temps = new double[100]; 
+        
+        return temps; 
+    }
 
 }
