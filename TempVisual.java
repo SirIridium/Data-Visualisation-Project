183f0ff1a2dfe896fcd70b1c6518273fa0bd4129
Cinderblock123 <153293909+DeepFryer123@users.noreply.github.com>
2024-03-14 10:02:56 -0400

    Update TempVisual.java

--- TempVisual.java
+++ TempVisual.java
@@ -1,24 +1,17 @@
 import org.code.theater.*;
 import org.code.media.*;
 
-
+amongus
 public class TempVisual extends Scene{
 
-  /* Draws a line on the graph as a function of:
-   * the value put in as the High temperature for the first line
-   * as the space from the bottom of the graph
-   * and the value put in for the second line is low temperature 
-   * this generates a line that looks like one of those statisitcal plots 
-   */
-
-  public void drawGraph(double[][] data){
-    int spacing=0;
-    for(int i=0; i<data.length-1;i++){
-
-      drawLine(spacing, 400 - (int)data[i][0], spacing+ 30, 400 - (int) data[i+1][0]);
-      drawLine(spacing, 400 - (int)data[i][1], spacing+ 30, 400 - (int) data[i+1][1]);
-      
-      spacing+=30;
-    }
+public void drawGraph(double[][] data){
+  int spacing=0;
+  int space=400/data[0].length;
+  for(int i=0; i<data.length-1;i++){
+    drawLine(spacing,(400-(int)data[i][0]*2)-100, spacing+space,(400-(int)data[i+1][0]*2)-100);
+    drawLine(spacing,(400-(int)data[i][1]*2)-100,spacing+space,(400-(int)data[i+1][1]*2)-100);
+    spacing+=space;
   }
+}
+  
 }
