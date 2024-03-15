4d277e0d588cb782a958387fd27230a7162e1a05
Cinderblock123 <153293909+DeepFryer123@users.noreply.github.com>
2024-03-15 08:51:07 -0400

    Update TempVisual.java

--- TempVisual.java
+++ TempVisual.java
@@ -1,17 +1,17 @@
-import org.code.theater.*;
+   import org.code.theater.*;
 import org.code.media.*;
 
-amongus
-public class TempVisual extends Scene{
 
+public class TempVisual extends Scene{
 public void drawGraph(double[][] data){
   int spacing=0;
   int space=400/data[0].length;
+  int scale=1;
   for(int i=0; i<data.length-1;i++){
-    drawLine(spacing,(400-(int)data[i][0]*2)-100, spacing+space,(400-(int)data[i+1][0]*2)-100);
-    drawLine(spacing,(400-(int)data[i][1]*2)-100,spacing+space,(400-(int)data[i+1][1]*2)-100);
+    drawLine(spacing,(400-(int)data[i][0]*scale)-200, spacing+space,(400-(int)data[i+1][0]*scale)-200);
+    drawLine(spacing,(400-(int)data[i][1]*scale)-200,spacing+space,(400-(int)data[i+1][1]*scale)-200);
     spacing+=space;
   }
+    drawImage("bird.png",100,200,300);
 }
-  
 }
