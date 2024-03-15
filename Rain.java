781c10757aa23f9c3f7480ec68dbb483db2bf028
JPL45 <133790178+JPL45@users.noreply.github.com>
2024-03-15 09:23:18 -0400

    magger

--- Rain.java
+++ Rain.java
@@ -5,5 +5,48 @@ Public class Temp extends Display (
     ^ Same with snow 
     */
     
+    private double rain;
+    private double snow;
+    public Rain(double rain){
+        this.rain = rain;
+        this.snow=snow;
+  
+      }
+
+      public static double[][] createRain(String rainFile, String snowFile){
+        double[] rain = FileReader.toDoubleArray(rainFile);
+        double[] snow = FileReader.toDoubleArray(snowFile);
+        double[][] dataSet = new double[rain.length][2]; 
     
+        for(int i= 0;i<rain.length; i++){
+          dataSet[i][0]=rain[i];
+          dataSet[i][1]=snow[i];
+        }
+      return dataSet;
+}
+
+public double getRain(){
+    return rain; 
+}
+public double getSnow(){
+    return snow; 
+}
+public int getDay(){
+  return day; 
+}
+
+
+
+public static String rainToString(double[][] rainGraph2){
+    String result = ""; 
+    for(int row = 0; row < rainGraph2.length; row++){
+      result += "Day " + (row+1) + " ";
+        for(int col=0; col <= 1; col++){
+            result+= " | " + rainGraph2[row][col] +" | ";
+        }
+        result += "\n";
+    }
+    return result;
+}
+
 )
