79d0a79da40c44137cd7581c8d8d9d6caf47648f
JPL45 <133790178+JPL45@users.noreply.github.com>
2024-03-15 08:55:51 -0400

    Added comments for rain class

--- Rain.java
+++ Rain.java
@@ -1,5 +1,9 @@
-public class Rain extends Display {
-
-
+Public class Temp extends Display (
+    /*This is the class that will represent the rain/snowfall over the years
+    ----------------------------------------------------------------
+    Rain will be measured in CM per month for 10 years
+    ^ Same with snow 
+    */
     
-}
+    
+)
