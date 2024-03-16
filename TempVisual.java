aed8cb5ac98c65f0f41709a3616b64576e042c61
Cinderblock123 <153293909+DeepFryer123@users.noreply.github.com>
2024-03-15 23:33:54 -0400

    Update TempVisual.java

--- TempVisual.java
+++ TempVisual.java
@@ -1,33 +1,18 @@
 import org.code.theater.*;
 import org.code.media.*;
 
-public class TempVisual extends Display{
-  
-  private int days = 0; 
-
-  public TempVisual(int days){
-    this.days = days; 
-  }
-
-public void drawGraph(double[][] data){
-  
-  int spacing=0;
-  int space=400/data[0].length;
-  int spacing=0;
-
-  int space=400/days;
-  int scale=1;
-  for(int i=0; i<days;i++){
-    if(days==1){
-      drawLine(spacing, (400 - (int) data[0][0] * scale) - 200, spacing + space,(400 - (int) data[0][0] * scale) - 200);
-      drawLine(spacing, (400 - (int) data[0][1] * scale) - 200, spacing + space,(400 - (int) data[0][1] * scale) - 200);
+public class TempVisual extends Scene {
+    public void drawGraph(double[][] data, int days) {
+        double spacing = 0.0; 
+        double space = 400.0 / (double)(days); 
+        int scale = 3;
+        int shift = 100;
+//amongus
+        for (int i = 0; i < days; i++) {
+            drawLine((int)spacing, (400 - (int)data[i][0] * scale) - shift, (int)(spacing + space), (400 - (int)data[i + 1][0] * scale) - shift);
+            drawLine((int)spacing, (400 - (int)data[i][1] * scale) - shift, (int)(spacing + space), (400 - (int)data[i + 1][1] * scale) - shift);
+            spacing += space;
+        }
+        drawImage("bird.png", 100, 200, 300);
     }
-    else{
-    drawLine(spacing,(400 - (int)data[i][0] * scale) - 200, spacing + space,(400 - (int) data[i+1][0] * scale) -200);
-    drawLine(spacing,(400 - (int) data[i][1] * scale) - 200, spacing + space,(400 - (int) data[i+1][1] * scale) - 200);
-    spacing += space;
-    }
-  }
-    drawImage("bird.png", 0, 0, 400);
-}
 }
