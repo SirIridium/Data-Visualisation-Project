13c600c55da23b3a1678667398ceb758bdbc419d
Cinderblock123 <153293909+DeepFryer123@users.noreply.github.com>
2024-03-18 11:35:54 -0400

    Update TempVisual.java

--- TempVisual.java
+++ TempVisual.java
@@ -7,12 +7,22 @@ public class TempVisual extends Scene {
         double space = 400.0 / (double)(days); 
         int scale = 3;
         int shift = 100;
-//amongus
-        for (int i = 0; i < days; i++) {
-            drawLine((int)spacing, (400 - (int)data[i][0] * scale) - shift, (int)(spacing + space), (400 - (int)data[i + 1][0] * scale) - shift);
-            drawLine((int)spacing, (400 - (int)data[i][1] * scale) - shift, (int)(spacing + space), (400 - (int)data[i + 1][1] * scale) - shift);
+if(days==1){
+setStrokeColor("red");
+             drawLine((int)spacing, (400 - (int)data[0][0] * scale) - shift, (int)(spacing + space), (400 - (int)data[0][0] * scale) - shift);
+   setStrokeColor("blue");
+           drawLine((int)spacing, (400 - (int)data[0][1] * scale) - shift, (int)(spacing + space), (400 - (int)data[0][1] * scale) - shift);
+        }
+      else{
+        for (int i = 1; i < days-1; i++) { 
+       setStrokeColor("red");
+          drawLine((int)spacing, (400 - (int)data[i][0] * scale) - shift, (int)(spacing + space), (400 - (int)data[i + 1][0] * scale) - shift);
+       setStrokeColor("blue");
+          drawLine((int)spacing, (400 - (int)data[i][1] * scale) - shift, (int)(spacing + space), (400 - (int)data[i + 1][1] * scale) - shift);
             spacing += space;
         }
+      }
+      playSound("amongus-sound.wav");
         drawImage("bird.png", 100, 200, 300);
     }
 }
