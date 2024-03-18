8772ac40695d5cd52d80131dbc3ae3b8b61043ff
JPL45 <133790178+JPL45@users.noreply.github.com>
2024-03-18 11:46:38 -0400

    CBT

---
+++ Precipitation.java/Precipitation.java
@@ -0,0 +1,54 @@
+Public class Precipitation(
+    /*This is the class that will represent the rain/snowfall over the years
+    ----------------------------------------------------------------
+    Rain will be measured in CM per month for 10 years
+    ^ Same with snow 
+    THATS GONNA TAKE FOREVER 
+    check issues. 
+    */
+    
+    private double rain;
+    private double snow;
+    public Precipitation(double rain, double snow){
+        this.rain = rain;
+        this.snow=snow;
+  
+      }
+
+      public static double[][] createRain(String rainFile, String snowFile){
+        double[] rain = FileReader.toDoubleArray(rainFile);
+        double[] snow = FileReader.toDoubleArray(snowFile);
+        double[][] dataSet = new double[rain.length][2]; 
+    
+        for(int i= 0;i<rain.length; i++){
+          dataSet[i][0]=rain[i];
+          dataSet[i][1]=snow[i];
+        }
+      return dataSet;
+}
+
+public double getRain(){
+    return rain; 
+}
+public double getSnow(){
+    return snow; 
+}
+public int getDay(){
+  return day; 
+}
+
+
+
+public static String precipitationToString(double[][] rainGraph2){
+    String result = ""; 
+    for(int row = 0; row < rainGraph2.length; row++){
+      result += "Day " + (row+1) + " ";
+        for(int col=0; col <= 1; col++){
+            result+= " | " + rainGraph2[row][col] +" | ";
+        }
+        result += "\n";
+    }
+    return result;
+}
+
+)
