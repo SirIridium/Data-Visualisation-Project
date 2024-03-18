093d79451c4205146e2f395b3b4c03b168a5db03
JPL45 <133790178+JPL45@users.noreply.github.com>
2024-03-18 12:08:09 -0400

    e

---
+++ Graphicals/Greenhouse.java
@@ -0,0 +1,14 @@
+public class {
+/* This is the class that will measure the CO2 levels and plastic levels in the ocean  
+ * ________________________________________________________________
+ * TODO:
+ * LUKE - get Tonnes CO2 and Tonnes Plastic per month for 10 years 
+ * -DF123 : Make visual for this and KYS (Keep yourself safe)
+*/
+
+
+    private double tonnesCO2;
+    private double tonnesPlastic;
+
+
+}
