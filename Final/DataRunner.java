4a5fe8b8e00d7bb7d96c3287633a0f37ff7b0a78
Cinderblock123 <153293909+DeepFryer123@users.noreply.github.com>
2024-03-19 23:05:35 -0400

    Update DataRunner.java

--- Final/DataRunner.java
+++ Final/DataRunner.java
@@ -1,1 +1,52 @@
+import org.code.media.*;
+import org.code.theater.*;
+import java.util.Scanner;
+public class DataRunner {
+    public static void main(String[] args){
+        Scanner userChoice = new Scanner(System.in);
+         System.out.println("\n\n\nData Visusalisation project on climate change.\n\n\n");
+        /* we are going to be using the user choice
+         * This is going to check what the user is picking for their graph / years they want to see
+         * We are going to build the conditionals as a debugging structure, 
+         * however we need to decide what we are doing for the project.
+         */
+        System.out.println("Please state which graph you would like to see: CO2, Rain, Temp"); 
+        String choice = userChoice.nextLine();
+        
+        
+        if(choice.equals("CO2")){
+          int days=0;
+            System.out.println("How many years would you like to go back?");
+            days=userChoice.nextInt();
+          CarbonVisual thing= new CarbonVisual();
+     thing.drawGraph(FileReader.toDoubleArray("tons.txt"),days);
+      Theater.playScenes(thing);
+            }
 
+        
+
+        if(choice.equals("Rain")){
+          int month=0;
+            System.out.println("What month would you like to see?");
+            month=userChoice.nextInt();
+      WaterVisual thing= new WaterVisual();
+     thing.drawGraph(FileReader.toDoubleArray("wet.txt"),FileReader.toDoubleArray("dry.txt"),month);
+      Theater.playScenes(thing);
+            }
+
+        
+
+        if(choice.equals("Temp") || choice.equals("temp")){
+          int days=0;
+            System.out.println("How many ({days} or {years}) would you like to go back?");
+            days=userChoice.nextInt();
+          TempVisual thing= new TempVisual();
+     thing.drawGraph(Temp.createTemp("tempHigh.txt","tempLow.txt"),days);
+      Theater.playScenes(thing);
+            }
+
+        userChoice.close();
+      
+        }
+
+}
