68185e861f3b9dfc39a266eb8a99070b6d6bf6e2
SirIridium <Luke.a.bushell@gmail.com>
2024-03-20 08:21:01 -0400

    comment

--- Final/Graph/Carbon.java
+++ Final/Graph/Carbon.java
@@ -4,11 +4,9 @@ import org.code.media.*;
 
 
 public class Carbon{
-/* This is the class that will measure the CO2 levels and plastic levels in the ocean  
+/* This is the class that will measure the CO2 levels
  * ----------------------------------------------------------------
- * TODO:
- * LUKE - get Tonnes CO2 and Tonnes Plastic per month for 10 years 
- * -DF123 : Make visual for this and KYS (Keep yourself safe)
+ * This is used for the CO2 Graph
 */
   
       /* Creates and returns a 2d array of doubles
